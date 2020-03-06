2cb182be9d1c477b7efc86f184607ad52c947072
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2020-03-06 01:19:15 -0600

    Prevent showing current filename

--- js/elements.js
+++ js/elements.js
@@ -1,5 +1,5 @@
 export const canvas = document.getElementById('canvas')
 export const ctx = canvas.getContext('2d')
-export const fileUpload = document.getElementById('fileUpload')
+export const fileUpload = document.getElementById('file-upload')
 export const rangeContainer = document.getElementById('range-container')
 export const moveSelector = document.getElementById('move-selector')
