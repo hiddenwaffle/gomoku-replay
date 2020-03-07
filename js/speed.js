b5fa4a1ded9a991b4e54449646f563eb913d5b9c
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2020-03-07 16:49:16 -0600

    Add speed editor

--- js/speed.js
+++ js/speed.js
@@ -8,37 +8,83 @@ import {
 const DEFAULT_DELAY = 333
 
 export function init() {
-  resetDelay(DEFAULT_DELAY)
-  speedDiv.addEventListener('click', handleClick)
+  speedDiv.addEventListener('keydown', handleKeyDown)
+  speedDiv.addEventListener('blur', handleBlur)
+  setPlaybackSpeed(1)
 }
 
-function handleClick() {
-  const result = prompt('Enter your preferred ms/frame delay\n' +
-                        '333 is the default:',
-                        getDelay().toString())
-  const enteredValue = parseFloat(result)
-  let delay
-  if (enteredValue > 0) {
-    delay = enteredValue
-  } else {
-    delay = DEFAULT_DELAY
+/**
+ * Content editable code based on:
+ * http://html5doctor.com/the-contenteditable-attribute/
+ * http://jsbin.com/owavu3/1/edit?html,js,output
+ * https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code/code_values
+ */
+function handleKeyDown(event) {
+  if (event.code === 'Escape') {
+    document.execCommand('undo')
+    speedDiv.blur()
+  } else if (event.code === 'Enter' ||
+             event.code === 'NumpadEnter') {
+    speedDiv.blur()
+    event.stopPropagation() // Do not interfere with window controls TODO: Check true
+    event.preventDefault()
   }
-  resetDelay(delay)
 }
 
-function resetDelay(value) {
-  setDelay(value)
-  updateDisplay(value)
+function handleBlur(event) {
+  const entered = speedDiv.innerText
+  const asNumber = parseFloat(entered)
+  if (asNumber > 0) {
+    setPlaybackSpeed(asNumber)
+  }
+}
+
+function setPlaybackSpeed(playbackSpeed) {
+  if (playbackSpeed < 1) {
+    speedDiv.innerText = `${playbackSpeed.toFixed(2)}x`
+  } else {
+    speedDiv.innerText = `${playbackSpeed}x`
+  }
+  setDelay(DEFAULT_DELAY / playbackSpeed)
   eventBus.fire('speed-changed')
 }
 
-function updateDisplay(delay) {
-  const ratio = DEFAULT_DELAY / delay
-  let ratioStr
-  if (ratio >= 1) {
-    ratioStr = Math.round(ratio).toString()
-  } else {
-    ratioStr = ratio.toFixed(2)
-  }
-  speedDiv.innerText = `${ratioStr}x`
-}
+// export function init() {
+//   resetDelay(DEFAULT_DELAY)
+//   speedDiv.addEventListener('click', handleClick)
+// }
+
+// function handleClick() {
+//   const currentRatio = DEFAULT_DELAY / getDelay()
+//   const result = prompt('Enter your preferred playback speed\n' +
+//                         '0.5 = half speed\n' +
+//                         '1 = normal\n' +
+//                         '2 = twice as fast\n' +
+//                         'etc...',
+//                         currentRatio.toString())
+//   const enteredValue = parseFloat(result)
+//   let delay
+//   if (enteredValue && enteredValue > 0) {
+//     delay = DEFAULT_DELAY / enteredValue
+//   } else {
+//     delay = currentRatio
+//   }
+//   resetDelay(delay)
+// }
+
+// function resetDelay(value) {
+//   setDelay(value)
+//   updateDisplay(value)
+//   eventBus.fire('speed-changed')
+// }
+
+// function updateDisplay(delay) {
+//   const ratio = DEFAULT_DELAY / delay
+//   let ratioStr
+//   if (ratio >= 1) {
+//     ratioStr = Math.round(ratio).toString()
+//   } else {
+//     ratioStr = ratio.toFixed(2)
+//   }
+//   speedDiv.innerText = `${ratioStr}x`
+// }
