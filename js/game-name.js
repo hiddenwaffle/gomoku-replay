06d1922696f6db50c133e6be6d8048ff7894c242
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2020-03-07 13:06:23 -0600

    Make prettier

--- js/game-name.js
+++ js/game-name.js
@@ -1,11 +1,62 @@
-import {gameNameSpan} from './elements'
+import {
+  gameNameDiv,
+  gameNameFancyDiv,
+  redNameSpan,
+  blueNameSpan,
+  messageDiv
+} from './elements'
 import {eventBus} from './event-bus'
-import {gameName} from './model'
+import {gameName as getGameName} from './model'
 
 export function init() {
   eventBus.register('game-name-updated', displayGameName)
 }
 
+/**
+ * Example log line:
+ * 2020-03-07 11:23:10,913 - bob.v.alice - INFO - Winner: 1 in 15 moves time= 0.650 secs
+ * Assumptions:
+ *  - 'bob' is red/X
+ *  - 'alice' is blue/O
+ */
+function parse(line) {
+  const matchResults = line.match(/^(.*) (.*) - (.*)\.v\.(.*) - INFO - Winner: (.*) in (.*) moves time= (.*)$/)
+  if (!matchResults || matchResults.length !== 8) {
+    return [false]
+  }
+  // Remember [0] is the whole string again
+  // const date = matchResults[1]
+  // const time = matchResults[2]
+  const red = matchResults[3]
+  const blue = matchResults[4]
+  const winner = matchResults[5] // 1, -1, or 0
+  const numMoves = matchResults[6]
+  const elapsed = matchResults[7]
+  let message
+  if (winner === '1') {
+    message = `${red} wins in ${numMoves} moves over ${elapsed}`
+  } else if (winner === '-1') {
+    message = `${blue} wins in ${numMoves} moves over ${elapsed}`
+  } else {
+    message = `TIE - ${numMoves} moves over ${elapsed}`
+  }
+  return [true, red, blue, message]
+}
+
 function displayGameName() {
-  gameNameSpan.innerText = gameName()
+  const line = getGameName()
+  const [success, red, blue, message] = parse(line)
+  if (success) {
+    gameNameDiv.style.display = 'none'
+    gameNameFancyDiv.style.display = 'block'
+    messageDiv.style.display = 'block'
+    redNameSpan.innerText = red
+    blueNameSpan.innerText = blue
+    messageDiv.innerText = message
+  } else {
+    gameNameDiv.style.display = 'block'
+    gameNameFancyDiv.style.display = 'none'
+    messageDiv.style.display = 'none'
+    gameNameDiv.innerText = line
+  }
 }
